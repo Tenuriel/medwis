c48dc65470e02e91f012b9b05017c0b9ef867317
Martin Zurowietz <mouagip@nexus-der-macht.de>
2014-07-09 20:30:08 +0200

    First version of similarity input file reading (categories file and matrices files). Binary and matrix similarity work, too.

--- medwis/src/main/java/main/Similarity.java
+++ medwis/src/main/java/main/Similarity.java
@@ -1,85 +1,229 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package main;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Map.Entry;
 import org.yaml.snakeyaml.Yaml;
+import au.com.bytecode.opencsv.CSVReader;
 
 /**
  *
  * @author Martin Zurowietz
  */
 public class Similarity {
-   
-   private static LinkedHashMap<String, LinkedHashMap<String, Object>> categories;
-   
-   private static final Yaml YAML = new Yaml();
-   
+
+   /**
+    * Maps every category to its similarity computing type.
+    */
+   private static HashMap<String, String> types;
+
+   /**
+    * Maps every category of type "gauss" to its male standard deviation.
+    */
+   private static HashMap<String, Float> standardDeviationsMale;
+
+   /**
+    * Maps every category of type "gauss" to its female standard deviation.
+    */
+   private static HashMap<String, Float> standardDeviationsFemale;
+
+   /**
+    * Maps every category of type "matrix" to its male similarity matrix.
+    */
+   private static HashMap<String, Similarity.Matrix> matricesMale;
+
+   /**
+    * Maps every category of type "matrix" to its female similarity matrix.
+    */
+   private static HashMap<String, Similarity.Matrix> matricesFemale;
+
    static {
-      Similarity.categories = Similarity.loadCategories();
-      Similarity.cacheMatrices();
+      Similarity.types = new HashMap<>();
+      Similarity.standardDeviationsFemale = new HashMap<>();
+      Similarity.standardDeviationsMale = new HashMap<>();
+      Similarity.matricesFemale = new HashMap<>();
+      Similarity.matricesMale = new HashMap<>();
+
+      try {
+         Similarity.init();
+      } catch (IOException ex) {
+         GUI.console.append("Initializing of the similarity computing class failed!" + "\n");
+         GUI.console.append(ex.getMessage() + "\n");
+      }
    }
-   
-   private Similarity() {}
-   
+
+   private Similarity() {
+   }
+
+   /**
+    * Computes the similarity of two cases.
+    *
+    * @param inputCase Case as provided by the user.
+    * @param storedCase Case from the data-basis to compare.
+    * @return Similarity of the cases.
+    */
    public static float compute(HashMap<String, String> inputCase, HashMap<String, String> storedCase) {
+      //TODO
       return 0;
    }
-   
-   private static float getSimilarityOf(String categoryName, String inputValue, String storedValue) {
-      LinkedHashMap<String, Object> category = Similarity.categories.get(categoryName);
-      String type = (String) category.get("type");
+
+   /**
+    * Calculates the similarity of two values of a given category of two cases.
+    *
+    * @param isFemale Is the input case female? Else male.
+    * @param categoryName The category of the values to compare.
+    * @param inputValue The value of the input case.
+    * @param storedValue The value of the case from the data-basis.
+    * @return The similarity of the two values.
+    */
+   private static float getSimilarityOf(boolean isFemale, String categoryName, String inputValue, String storedValue) {
+      String type = Similarity.types.get(categoryName);
       float similarity = 0;
-      
+
       if (type.equals("gauss")) {
-         similarity = Similarity.getGaussSimilarity(category, inputValue, storedValue);
+         similarity = Similarity.getGaussSimilarity(isFemale, categoryName, inputValue, storedValue);
       } else if (type.equals("matrix")) {
-         similarity = Similarity.getMatrixSimilarity(category, inputValue, storedValue);
+         similarity = Similarity.getMatrixSimilarity(isFemale, categoryName, inputValue, storedValue);
       } else {
          similarity = Similarity.getBinarySimilarity(inputValue, storedValue);
       }
-      
+
       return similarity;
    }
-   
-   private static float getGaussSimilarity(LinkedHashMap<String, Object> category, String inputValue, String storedValue) {
-      return 0;
-   }
-   
-   private static float getMatrixSimilarity(LinkedHashMap<String, Object> category, String inputValue, String storedValue) {
-      return 0;
-   }
-   
-   private static float getBinarySimilarity(String inputValue, String storedValue) {
-      if (inputValue.equals(storedValue)) return 1;
-      return 0;
-   }
-   
-   private static LinkedHashMap<String, LinkedHashMap<String, Object>> loadCategories() {
-      InputStream input = null;
-      LinkedHashMap<String, LinkedHashMap<String, Object>> output;
-      try {
-         input = new FileInputStream(new File("categories.yml"));
-      } catch (FileNotFoundException ex) {
-         Logger.getLogger(Similarity.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      output = (LinkedHashMap<String, LinkedHashMap<String, Object>>) YAML.load(input);
-      return output;
-   }
-   
-   private static void cacheMatrices() {
+
+   /**
+    * Calculates the similarity of two values of a category by the gaussian
+    * method.
+    *
+    * @param isFemale Is the input case female? Else male.
+    * @param categoryName The category of the values to compare.
+    * @param inputValue The value of the input case.
+    * @param storedValue The value of the case from the data-basis.
+    * @return The similarity of the two values.
+    */
+   private static float getGaussSimilarity(boolean isFemale, String categoryName, String inputValue, String storedValue) {
       //TODO
+      return 0;
    }
-   
+
+   /**
+    * Calculates the similarity of two values of a category by the matrix
+    * method.
+    *
+    * @param isFemale Is the input case female? Else male.
+    * @param categoryName The category of the values to compare.
+    * @param inputValue The value of the input case.
+    * @param storedValue The value of the case from the data-basis.
+    * @return The similarity of the two values.
+    */
+   private static float getMatrixSimilarity(boolean isFemale, String categoryName, String inputValue, String storedValue) {
+      return (isFemale)
+              ? Similarity.matricesFemale.get(categoryName).get(inputValue, storedValue)
+              : Similarity.matricesMale.get(categoryName).get(inputValue, storedValue);
+   }
+
+   /**
+    * Calculates the similarity of two values of a category by the binary
+    * method.
+    *
+    * @param inputValue The value of the input case.
+    * @param storedValue The value of the case from the data-basis.
+    * @return The similarity of the two values.
+    */
+   private static float getBinarySimilarity(String inputValue, String storedValue) {
+      return (inputValue.equals(storedValue)) ? 1 : 0;
+   }
+
+   private static void init() throws IOException {
+      InputStream input = null;
+      Yaml yaml = new Yaml();
+
+      input = new FileInputStream(new File("categories.yml"));
+
+      // temporary internal representation of the categories Yaml file
+      LinkedHashMap<String, LinkedHashMap<String, Object>> categories
+              = (LinkedHashMap<String, LinkedHashMap<String, Object>>) yaml.load(input);
+
+      Iterator<Entry<String, LinkedHashMap<String, Object>>> iterator
+              = categories.entrySet().iterator();
+
+      while (iterator.hasNext()) {
+         Entry<String, LinkedHashMap<String, Object>> entry = iterator.next();
+         String category = entry.getKey();
+
+         String type = (String) entry.getValue().get("type");
+         String valueFemale = (String) entry.getValue().get("f_value");
+         String valueMale = (String) entry.getValue().get("m_value");
+
+         Similarity.types.put(category, type);
+
+         if (type.equals("gauss")) {
+            Similarity.standardDeviationsFemale.put(category, new Float(valueFemale));
+            Similarity.standardDeviationsMale.put(category, new Float(valueMale));
+         } else if (type.equals("matrix")) {
+            Similarity.matricesFemale.put(category, new Similarity.Matrix(valueFemale));
+            Similarity.matricesMale.put(category, new Similarity.Matrix(valueMale));
+         }
+      }
+   }
+
+   /**
+    * Class to represent a similarity-matrix.
+    */
+   private static class Matrix {
+
+      /**
+       * The categories (labels) of the rows and columns (rows and columns are
+       * labeled the same).
+       */
+      private ArrayList<String> categories;
+      /**
+       * The array of similarity values.
+       */
+      private float[][] values;
+
+      public Matrix(String path) throws IOException {
+         CSVReader csv = null;
+         String[] line = null;
+         int lineNumber = 0;
+
+         // read tab-separated file
+         csv = new CSVReader(new FileReader(path), '\t');
+         line = csv.readNext();
+         int dimension = line.length;
+
+         this.values = new float[dimension][dimension];
+
+         this.categories = new ArrayList(Arrays.asList(line));
+
+         while ((line = csv.readNext()) != null) {
+            for (int i = 0; i < dimension; i++) {
+               this.values[lineNumber][i] = Float.parseFloat(line[i]);
+            }
+            lineNumber++;
+         }
+      }
+
+      /**
+       * Returns the similarity of firstCategory and secondCategory as given by
+       * this similarity matrix. Does no error-checks for performance reasons.
+       * So make sure the categories are valid!
+       *
+       * @param firstCategory
+       * @param secondCategory
+       * @return The similarity of the two categories as given by this
+       * similarity matrix.
+       */
+      public float get(String firstCategory, String secondCategory) {
+         return this.values[this.categories.indexOf(firstCategory)][this.categories.indexOf(secondCategory)];
+      }
+   }
 }
